4101df2f906f8540526ee240c97362fa508a2f75
copium <79623528+AzSumPesho@users.noreply.github.com>
2024-12-12 22:25:17 +0200

    Update DelayPackets.java

--- src/main/java/addon/antip2w/modules/DelayPackets.java
+++ src/main/java/addon/antip2w/modules/DelayPackets.java
@@ -1,8 +1,12 @@
 package addon.antip2w.modules;
-
+//todo: actually make this decent
 import meteordevelopment.meteorclient.events.game.GameLeftEvent;
 import meteordevelopment.meteorclient.events.game.OpenScreenEvent;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import meteordevelopment.meteorclient.utils.network.PacketUtils;
+import meteordevelopment.meteorclient.settings.PacketListSetting;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
@@ -11,9 +15,19 @@ import net.minecraft.network.packet.Packet;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 public class DelayPackets extends Module {
     private final Queue<Packet<?>> delayedPackets = new LinkedList<>();
+
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final Setting<Set<Class<? extends Packet<?>>>> c2sPackets = sgGeneral.add(new PacketListSetting.Builder()
+        .name("C2S-packets")
+        .description("Client-to-server packets to delay.")
+        .filter(aClass -> PacketUtils.getC2SPackets().contains(aClass))
+        .build()
+    );
+
 
     public DelayPackets() {
         super(Categories.DEFAULT, "delay-packets", "Delays C2S packets when turned on.");
@@ -21,8 +35,12 @@ public class DelayPackets extends Module {
 
     @EventHandler(priority = EventPriority.HIGHEST + 1)
     private void onSendPacket(PacketEvent.Send event) {
-        delayedPackets.add(event.packet);
-        event.cancel();
+        if (c2sPackets.get().contains(event.packet.getClass())) {
+            synchronized (delayedPackets) {
+                delayedPackets.add(event.packet);
+            }
+            event.cancel();
+        }
     }
 
     @EventHandler
